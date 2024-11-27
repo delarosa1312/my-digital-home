f5f50f0d57a7ae849b1a7ab7cf41fd7e39dbeb1d
Matheus Dela Rosa <delarosamonteiro@gmail.com>
2024-11-27 19:07:34 +0100

    Feat: Refactor App component to use Material-UI and implement Header, Content, and Footer; integrate theme context for dark mode support

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
-import generateRoutes from "./utils/generateRoutes";
-import React from "react";
-import About from "./pages/About";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Grid2, CssBaseline, ThemeProvider as MuiThemeProvider } from "@mui/material";
+import { useContext } from "react";
+import Header from "./components/common/Header";
+import Content from "./components/common/Content";
+import Footer from "./components/common/Footer";
+import { lightTheme, darkTheme } from "./theme";
+import { ThemeContext } from "./themeContext";
+import "./index.css";
 
 function App() {
-  const routes = generateRoutes();
+  const { isDarkMode } = useContext(ThemeContext);
 
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        {routes.map((route, index) => (
-          <Route
-            key={index}
-            path={route.path}
-            element={route.element ? React.createElement(route.element) : null}
-          />
-        ))}
-      </Routes>
-    </Router>
+    <MuiThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <CssBaseline />
+      <Router>
+        <Grid2 container spacing={2} padding={1} direction="column" sx={{ minHeight: "100vh" }}>
+          <Grid2 container size={{ xs: 12 }}>
+            <Header />
+          </Grid2>
+
+          <Grid2 container size={{ xs: 12 }} sx={{ flexGrow: 1, overflow: "auto" }}>
+            <Content />
+          </Grid2>
+
+          <Grid2 container size={{ xs: 12 }}>
+            <Footer />
+          </Grid2>
+        </Grid2>
+      </Router>
+    </MuiThemeProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// src/main.tsx
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ThemeProvider } from "./themeContext";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
-)
+);
